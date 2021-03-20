f70cd7ecb2560e423c1a5b2d4c8be6e337911c3e
JoseGabrielZeballos <a2019117789@estudiantes.upsa.edu.bo>
2021-03-20 10:38:14 -0400

    Se hizo de nuevo el proyecto,el modelo,el datacontext y el web config.

---
+++ sParcial1/Parcial1/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Parcial1.Models
+{
+    public class Product
+    {
+        [Key]
+        [Range(1, 99999)]
+        public int ProductID { get; set; }
+        [Required]
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "Se requiere descripcion del producto entre 30 y 3 caracteres")]
+
+        public string Description { get; set; }
+        [DataType(DataType.Currency)]
+        [Required]
+        public string Price { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime LastBuy { get; set; }
+
+    }
+}
