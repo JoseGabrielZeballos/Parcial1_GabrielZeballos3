8ab43092082629b422790c627969b4af09f2d1c2
JoseGabrielZeballos <a2019117789@estudiantes.upsa.edu.bo>
2021-03-20 11:39:24 -0400

    Se agrego SumitaController y el ProductsController

---
+++ sParcial1/Parcial1/Controllers/SumitaController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+namespace Parcial1.Controllers
+{
+    public class SumitaController : ApiController
+    {
+        [HttpPost]
+        public int Add ( int numa, int numb)
+            {
+                return numa + numb;
+
+            }
+    }
+}
